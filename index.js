12d518ecd69f4273406fb2521217031d9a316158
nkathamilcahmakini <nkathamilcah@gmail.com>
2023-05-30 18:48:39 +0300

    Created an informative website that has electronic accessories and used JavaScript to access APIs and added images to the website

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-function generateProductCards(data) {
-    const productContainer = document.getElementById("offers");
-    data.forEach((product) => {
-      const card = document.createElement("div");
-      card.classList.add("product-card");
-      card.innerHTML = `
-              <img src="${product.thumbnail}" alt="${product.name}" />
-              <h2>${product.title}</h2>
-              <div class='productPrice'>
-              <p>${product.price}</p>
-              <button>ADD TO CART</button>
-              </div>
-          `;
-      productContainer.appendChild(card);
-    });
-  }
-  function fetchProducts() {
-    fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
-      .then((data) => {
-        const returnedData = data.products.slice(0, 10);
-        generateProductCards(returnedData);
+function fetchProducts() {
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((information) => {
+      const returnProduct = information.products.slice(0, 8);
+      generateProductCards(returnProduct);
     });
 }
-fetchProducts()
 
-// #users{
-//   display: flex;
-//   flex-wrap: wrap;
-//   justify-content: space-evenly;
-// }
-// .people{
-//   border: solid aqua;
-//   border-radius: 5px;
-//   margin-bottom: 2%;
-//   text-align: center;
-// }
+function generateProductCards(information) {
+  const productsContainer = document.getElementById("offers");
+  for (const product of information) {
+    const classifiedProducts = document.createElement("div");
+    classifiedProducts.classList.add("prod-card");
+    classifiedProducts.innerHTML = `
+      <img src="${product.thumbnail}" alt="${product.name}" />
+      <h2>${product.title}</h2>
+      <div class='productPrice'>
+        <p>${product.price}</p>
+        <button>Add to Cart</button>
+      </div>
+    `;
+    productsContainer.appendChild(classifiedProducts);
+  }
+}
+
+fetchProducts();
+
